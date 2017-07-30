4de502cdc2ed489551f4563d0bf5e0f909e95cd8
Enes Anbar <enesanbar@gmail.com>
2017-07-30 18:44:12 +0300

    created the missing helper functions

--- setup.sh
+++ setup.sh
@@ -14,6 +14,11 @@ declare -a FILES_TO_SYMLINK=(
     'git/gitignore'
 )
 
+execute() {
+    $1 &> /dev/null
+    print_result $? "${2:-$1}"
+}
+
 answer_is_yes() {
     [[ "$REPLY" =~ ^[Yy]$ ]] \
         && return 0 \
@@ -24,6 +29,36 @@ ask_for_confirmation() {
     print_question "$1 (y/n) "
     read -n 1
     printf "\n"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m $1\e[0m\n\n"
 }
 
 main() {
@@ -58,3 +93,5 @@ main() {
 }
 
 main
+
+# TODO: Automate the installation of zsh && oh-my-zsh && bullet-train theme.
